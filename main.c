43a6c5f9b660cb3892f480bb1d9bb51ea1cd3c9a
Armando Correa <ie708826@iteso.mx>
2020-05-15 17:29:11 -0500

    msg queue added for mosi bit transmition.

--- main.c
+++ main.c
@@ -45,6 +45,7 @@
 #include "event_groups.h"
 #include "GPIO.h"
 #include "Bits.h"
+#include "queue.h"
 /* TODO: insert other definitions and declarations here. */
 
 /* Pin definitions */
@@ -63,17 +64,17 @@
 #define CLK_FALL_EDGE_EVENT			(1 << 2)
 
 /*** TASK PRIORITIES ***/
-#define TASK_CHIPSELECT_PRIO      	(configMAX_PRIORITIES-4)
+#define TASK_CHIPSELECT_PRIO      	(configMAX_PRIORITIES-2)
 #define TASK_CLK_PRIO      			(configMAX_PRIORITIES-3)
-#define TASK_MOSI_PRIO      		(configMAX_PRIORITIES-2)
+#define TASK_MOSI_PRIO      		(configMAX_PRIORITIES-4)
 
 typedef enum {LOW, HIGH} bit_t;
 
 typedef struct
 {
 	EventGroupHandle_t SPI_event;
+	QueueHandle_t mosiBit;
 	bit_t csBit;
-	bit_t mosiBit;
 }parameters_task_t;
 
 
@@ -107,12 +108,11 @@ int main(void) {
 
 
 
-    parameters_task_t parameters;
+    static parameters_task_t parameters;
     parameters.SPI_event =  xEventGroupCreate();
     parameters.csBit = LOW;
-    parameters.mosiBit = LOW;
+    parameters.mosiBit = xQueueCreate(8,sizeof(bit_t));
 
-    xEventGroupClearBits(parameters.SPI_event, (CHIPSELECT_EVENT | CLK_RISE_EDGE_EVENT | CLK_FALL_EDGE_EVENT));
 
     xTaskCreate(chipSelect_task, "chipSelect", 200, (void*) &parameters, TASK_CHIPSELECT_PRIO, NULL);
 	xTaskCreate(clk_task, "clk_task", 200, (void*) &parameters, TASK_CLK_PRIO, NULL);
@@ -151,26 +151,29 @@ void chipSelect_task(void* param)
 }
 void clk_task(void* param)
 {
-	static uint8_t newBitMask = (1<<0);
+	static uint8_t newBitMask = 1;
 	static bit_t clkState = LOW;
+	static uint8_t bit;
 	parameters_task_t parameters_task = *((parameters_task_t*) param);
 
+	xEventGroupWaitBits(parameters_task.SPI_event, CHIPSELECT_EVENT, pdFALSE, pdTRUE, portMAX_DELAY);
 
 	for(;;)
 	{
-		xEventGroupWaitBits(parameters_task.SPI_event, CHIPSELECT_EVENT, pdFALSE, pdTRUE, portMAX_DELAY);
-		switch (clkState)
+				switch (clkState)
 		{
 			case LOW:
 
 				if (newBitMask == 0x80)
 				{
 					newBitMask = 1;
-					parameters_task.mosiBit = (MOSI_BYTE && newBitMask);
+					bit = (MOSI_BYTE & newBitMask);
+					xQueueSend(parameters_task.mosiBit,&bit,portMAX_DELAY);
 				}
 				else
 				{
-					parameters_task.mosiBit = (MOSI_BYTE && newBitMask);
+					bit = (MOSI_BYTE & newBitMask);
+					xQueueSend(parameters_task.mosiBit,&bit,portMAX_DELAY);
 					newBitMask <<= 1;
 				}
 
@@ -198,12 +201,11 @@ void mosi_task(void* param)
 	bit_t bitToSend = LOW;
 	for(;;)
 	{
-		xEventGroupWaitBits(parameters_task.SPI_event, CLK_FALL_EDGE_EVENT, pdTRUE, pdTRUE, portMAX_DELAY);
-		bitToSend = parameters_task.mosiBit;
+		xQueueReceive(parameters_task.mosiBit, &bitToSend, portMAX_DELAY);
 		xEventGroupWaitBits(parameters_task.SPI_event, CLK_RISE_EDGE_EVENT, pdTRUE, pdTRUE, portMAX_DELAY);
 		if (lastBit != bitToSend)
 		{
-			GPIO_toogle_pin(SPI_PORT, CS_PIN);
+			GPIO_toogle_pin(SPI_PORT, MOSI_PIN);
 			lastBit = bitToSend;
 		}
 
